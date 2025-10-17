5edf2a245719652ac5e709cdd710388ab2e9df0e
Tamás Józsa <c0joztam@vasvari.org>
2025-10-17 14:26:26 +0200

    users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -39,9 +39,48 @@ router.post("/login", (req,res) => {
     if (!bcrypt.compareSync(password, user.password)) {
         res.status(400).json({ message: "Invalid" })
     }
-    const token = jwt.sign({id: user.id, email: user.email}, "secret_key", {expiresIn: "30m"}, )
-    res.status(201).json(token)
+    const token = jwt.sign({id: user.id, email: user.email}, "secret_key", {expiresIn: "30s"}, )
+    res.status(201).json( {token})
 })
+
+router.patch("/:id", (req, res) =>{
+    const id = req.userId
+    const {name, email, password} = req.body
+    let hashPassword
+    if (password) {
+        const salt = bcrypt.getSaltSync(12)
+        hashPassword = bcrypt.hashSync(password, salt)
+    }
+    let user = Users.getUserById(id)
+    Users.updateUser(id, name || user.name, email || user.email, hashPassword || user.password)
+    user = Users.getUserById(id)
+    delete user.password
+    res.status(201).json(user)
+})
+
+router.get("/me", (req, res) =>{
+    const user = Users.getUserById(+req.params)
+    delete user.password
+    res.json(user)
+})
+
+function auth(req, res, next) {
+    const accessToken = req.headers.authorization
+    if (!accessToken){
+        return res.status(400).json({ message: "Invalid" })
+    }
+    const token = accessToken.split(' ')[1]
+    const data = jwt.verify(token, "secret_key")
+    const now = Math.floor(Date.now() / 100)
+    if (data?.exp < now) {
+        return res.status(403).json({ message: "Invalid" })
+    }
+    req.userId = data.id
+    req.useremail = data.email
+    next()
+    console.log(accessToken)
+
+}
 
 
 export default router
